0728c7f3838115b31943c18b2e5819075e6208b7
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-23 01:17:49 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,22 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  if name_hash.length == 0
+    return nil
+  else
+    values = name_hash.collect { |k, v| v }
+    min = values[0]
+    values.each_with_index { |v, i|
+      if values[i] < min
+        min = values[i]
+      end
+      min
+    }
 
+    name_hash.each { |k,v|
+      if v == min
+        return k
+      end
+    }
+  end
 end
